c107adb1883f65d793f82e55db7e1f602ed8f1ae
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-05 13:23:13 -0400

    update deps

--- index.js
+++ index.js
@@ -23,15 +23,16 @@ util.inherits(Input, Prompt);
  * Render the prompt to terminal
  */
 
-Input.prototype.render = function(state) {
-  var append = typeof state === 'string' ? log.red('>> ') + state : '';
-  var message = this.message;
-  if (this.status === 'answered') {
-    message += log.cyan(this.answer);
-  } else {
-    message += this.rl.line;
-  }
-  this.ui.render(message, append);
+Input.prototype.render = function(err) {
+  var error = typeof err === 'string'
+    ? log.red('>> ') + err
+    : '';
+
+  var message = this.message + (this.status === 'answered'
+    ? log.cyan(this.answer)
+    : this.rl.line);
+
+  this.ui.render(message, error);
 };
 
 /**
